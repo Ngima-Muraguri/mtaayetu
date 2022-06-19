4bb101653dcfb93632f3dcb8f24b0b47173febc7
Ngima-Muraguri <milcah.muraguri@student.moringaschool.com>
2022-06-19 22:38:15 +0300

    added comments

--- hood/views.py
+++ hood/views.py
@@ -132,3 +132,19 @@ def create_post(request):
         else:
             form = PostForm()
         return render(request, 'posts/createpost.html', {"form": form})
+
+@login_required(login_url='/accounts/login/')
+def add_comment(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    current_user = request.user
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.poster = current_user
+            comment.save()
+            return redirect('home')
+    else:
+        form = CommentForm()
+        return render(request, 'comment.html', {"user": current_user, "comment_form": form})
